f6f126bedd900d04b13ef08500b2fc2023a6df27
Nathan-Coyne <nathan_coyne@hotmail.co.uk>
2021-12-13 19:24:38 +0000

    first commit

---
+++ src/resources/views/common_pages/modal.blade.php
@@ -0,0 +1,18 @@
+<!-- Modal -->
+<div class="modal fade" id="myModal">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                Title
+            </div>
+            <div class="modal-body">
+                Body
+            </div>
+            <div class="modal-footer">
+                <div id="modal-placeholder-response"></div>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <div id="modal-placeholder-button"></div>
+            </div>
+        </div>
+    </div>
+</div>
